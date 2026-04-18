a4a755e3a80a9712ae73ba74b4757973a7588709
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2026-04-17 18:57:27 -0700

    rename to Alms-Round

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Alms-Rounds/",
+  base: "/Alms-Round/",
   plugins: [
     react({
       babel: {
